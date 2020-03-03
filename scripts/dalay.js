28e2756901c834840e417bf0a48243d0fd10fa99
ilhov.space__home <ilhov.space@gmail.com>
2020-03-03 16:17:32 +0200

    delay task

---
+++ scripts/dalay.js
@@ -0,0 +1,8 @@
+function delay( ms ) {
+    return new Promise(resolve => setTimeout( () => resolve( ms ), ms ));
+  }
+
+
+/* Tests */
+
+delay(1000).then(value => console.log(`Done with ${value}`)); 
