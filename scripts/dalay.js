5b273e88a1f3e38c142dea51ad8650575923fd54
ilhov.space__home <ilhov.space@gmail.com>
2020-03-03 17:20:34 +0200

    delay in one line

--- scripts/dalay.js
+++ scripts/dalay.js
@@ -1,6 +1,4 @@
-const delay = (ms) => {
-    return new Promise(resolve => setTimeout(() => resolve(ms), ms));
-  }
+const delay = ms => new Promise(resolve => setTimeout(() => resolve(ms), ms));
 
 /* Tests */
 delay(1000).then(value => console.log(`Done with ${value}`));
