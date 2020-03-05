c9546a9b650c5ced50379dd0f07bdf5b9dd7abb7
ilhov.space__home <ilhov.space@gmail.com>
2020-03-05 15:59:54 +0200

    promise all task

---
+++ scripts/promiseAll.js
@@ -0,0 +1,16 @@
+const url = [
+    'http://www.json-generator.com/api/json/get/cevhxOsZnS',
+    'http://www.json-generator.com/api/json/get/cguaPsRxAi',
+    'http://www.json-generator.com/api/json/get/cfDZdmxnDm',
+    'http://www.json-generator.com/api/json/get/cfkrfOjrfS',
+    'http://www.json-generator.com/api/json/get/ceQMMKpidK'
+];
+const requestArray = url.map(item => {
+    return fetch(item).then(response => response.json())
+    })
+
+    console.log(requestArray)
+    
+Promise.all(requestArray)
+    .then(data => console.log(data))
+    .catch(error => console.log(error))
