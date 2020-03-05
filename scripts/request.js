040e90f453cbfb59f520b4c3d4ccbfecb35b4f8e
ilhov.space__home <ilhov.space@gmail.com>
2020-03-05 15:10:10 +0200

    request task update

--- scripts/request.js
+++ scripts/request.js
@@ -2,12 +2,13 @@ function request(){
     fetch('http://www.json-generator.com/api/json/get/cfQCylRjuG')
     .then(response => response.json())
     .then(data => {
-        if(data && data.getUsersData){
-            fetch('http://www.json-generator.com/api/json/get/cfVGucaXPC')
-            .then(response => response.json())
-            .then(data => console.log(data))
+        const {getUsersData} = data
+        if(getUsersData){
+          return fetch('http://www.json-generator.com/api/json/get/cfVGucaXPC')
         }
     })
+    .then(response => response.json())
+    .then(data => console.log(data))
     .catch(error => console.log(error))
 }
 request()
