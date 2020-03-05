d149bc5b5fc7a772414796194d95088657d5cf2b
ilhov.space__home <ilhov.space@gmail.com>
2020-03-05 16:59:54 +0200

    promise all updated

--- scripts/promiseAll.js
+++ scripts/promiseAll.js
@@ -5,9 +5,7 @@ const url = [
     'http://www.json-generator.com/api/json/get/cfkrfOjrfS',
     'http://www.json-generator.com/api/json/get/ceQMMKpidK'
 ];
-
-const requestArray = url.map(item => fetch(item).then(response => response.json()))
-    
-Promise.all(requestArray)
+ 
+Promise.all(url.map(item => fetch(item).then(response => response.json())))
     .then(data => console.log(data))
     .catch(error => console.log(error))
