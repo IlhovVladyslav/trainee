6705892deb30bad70eecab48844971a9376a804b
ilhov.space__home <ilhov.space@gmail.com>
2020-03-09 18:17:28 +0200

    my function updated

--- scripts/myFunctions.js
+++ scripts/myFunctions.js
@@ -7,16 +7,15 @@ Function.prototype.myBind = function(context, ...arg){return () => {this.call(co
 
 /*Tests*/
 //duplicate
-const DuplicateArray = [1, 2, 3].duplicate()
-console.log(DuplicateArray) // => [1, 2, 3, 1, 2, 3]
+console.log([1, 2, 3].duplicate()) // => [1, 2, 3, 1, 2, 3]
 
 //myCall
-const Person = {name: 'Ivan'}
-function PersonInfo(phone, city) {console.log('Name :', this.name, 'Phone :', phone, 'City :', city)}
-PersonInfo.myCall(Person, 1234, 'Kiev') // => Name : Ivan Phone : 1234 City : Kiev
+const person = {name: 'Ivan'}
+function personInfo(phone, city) {console.log('Name :', this.name, 'Phone :', phone, 'City :', city)}
+personInfo.myCall(person, 1234, 'Kiev') // => Name : Ivan Phone : 1234 City : Kiev
 
 //myApply
-PersonInfo.myApply(Person, [3454, 'Kharkiv']) // => Name : Ivan Phone : 3454 City : Kharkiv
+personInfo.myApply(person, [3454, 'Kharkiv']) // => Name : Ivan Phone : 3454 City : Kharkiv
 
 //myBind
-PersonInfo.myBind(Person, 7897, 'Sumy')() // => Name : Ivan Phone : 7897 City : Sumy
+personInfo.myBind(person, 7897, 'Sumy')() // => Name : Ivan Phone : 7897 City : Sumy
