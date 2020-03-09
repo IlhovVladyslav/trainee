db6f0e03874501b9fd7cc02633e837fd3496053e
ilhov.space__home <ilhov.space@gmail.com>
2020-03-09 19:22:10 +0200

    added props

--- scripts/objectCreatePolyfill.js
+++ scripts/objectCreatePolyfill.js
@@ -1,7 +1,14 @@
 if (!Object.create) {
-    Object.create = function(arg) {
+    Object.create = function(arg, props) {
       function Func() {}
       Func.prototype = arg;
+      if (typeof(props) === "object") {
+        for (prop in props) {
+         if (props.hasOwnProperty((prop))) {
+          Func[prop] = props[prop];
+         }
+        }
+       }
       return new Func();
     };
   }
