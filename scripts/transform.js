20cef48c385504f1b58cb35143c818114b2df175
ilhov.space__macbook <ilgov.space@gmail.com>
2020-03-16 14:35:18 +0200

    changed fransform func on arrow func

--- scripts/transform.js
+++ scripts/transform.js
@@ -1,9 +1,7 @@
-function transform(arg){
-    return arg.split('.').reverse().reduce((obj, i) => ({[i]: obj}), null);
-}
+const transform = arg => arg.split('.').reverse().reduce((result, next) => ({[next]: result}), null)
 
 /* Test */
 console.log(transform('a.b.c.d')) 
 
-// {a: {b: {c: {d: {}}}}}
+// {a: {b: {c: {d: null}}}
 
