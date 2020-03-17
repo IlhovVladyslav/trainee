ad702d33d1fa09d09389488eb97bc6ef641af12c
ilhov.space__macbook <ilgov.space@gmail.com>
2020-03-17 13:26:43 +0200

    added method has

--- scripts/union.js
+++ scripts/union.js
@@ -1,6 +1,12 @@
+// Тут тоже нужно сделать через Set, чтобы были уникальные элементы, и привести b к Set тоже,
+//  и использовать метод has вместо includes, так мы гарантируем уникальность в любом случае
+
+// Тут тоже заюзай has и преобразуй b к Set, и избавься от вложенности большой,
+//  зачем фильтр внутри аргумента new Set, когда можно сделать [...new Set(a)].filter(...
+
 const union = (a, b) => [...new Set([...a, ...b])]
-const intersection = (a, b) => a.filter(item => b.includes(item))
-const difference = (a, b) => [...new Set (a.filter(item => !b.includes(item)))]
+const intersection = (a, b) => [...new Set(a)].filter(item => new Set(b).has(item))
+const difference = (a, b) => [...new Set(a)].filter(item => !(new Set(b).has(item)))
 
 /* Test */
 console.log(union([4,5,7,2,1,5],[1,2,3,7,9])); // [4, 5, 7, 2, 1, 3, 9]
