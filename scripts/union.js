7e4f2b73c7773e1d17eb65992e6407fe14299f12
ilhov.space__macbook <ilgov.space@gmail.com>
2020-03-17 12:55:21 +0200

    union func init

---
+++ scripts/union.js
@@ -0,0 +1,9 @@
+const union = (a, b) => [...new Set([...a, ...b])]
+const intersection = (a, b) => a.filter(item => b.includes(item))
+const difference = (a, b) => [...new Set (a.filter(item => !b.includes(item)))]
+
+/* Test */
+console.log(union([4,5,7,2,1,5],[1,2,3,7,9])); // [4, 5, 7, 2, 1, 3, 9]
+console.log(intersection([1,2,3], [4,3,2])); // [2, 3]
+console.log(difference([1,2,3,7,9],[4,5,7,2,1,5])); // [3, 9]
+console.log(difference([4,5,7,2,1,5], [1,2,3,7,9])); // [4, 5]
